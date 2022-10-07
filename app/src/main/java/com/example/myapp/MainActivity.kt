5cedb44192186cff78122106b66efdc0dd0483b3
kandy <matus.kanda.po@gmail.com>
2022-10-07 14:31:47 +0200

    no idea what im doing

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -3,22 +3,20 @@ package com.example.myapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.MediaController
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.airbnb.lottie.LottieAnimationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val startButton: Button = findViewById(R.id.startButton)
-        val stopButton: Button = findViewById(R.id.stopButton)
-        val animation: LottieAnimationView = findViewById(R.id.animationView)
-        startButton.setOnClickListener {
-            animation.playAnimation()
-        }
-        stopButton.setOnClickListener {
-            animation.cancelAnimation()
-        }
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.firstFragment) as NavHostFragment
+        navController = navHostFragment.navController
+//
     }
 
 }
