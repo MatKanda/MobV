5f29dd0a8e6f8ec4518f52e50adf89dbb68a451a
MatKanda <75527738+MatKanda@users.noreply.github.com>
2022-10-28 14:13:43 +0200

    Add files via upload

---
+++ src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,71 @@
+package com.example.android.unscramble.ui.game
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel (){
+    private var _score = 0
+    val score: Int
+        get() = _score
+
+    private lateinit var _currentScrambledWord : String
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+
+    private var _currentWordCount = 0
+    val currentWordCount: Int
+        get() = _currentWordCount
+
+    private var usedWordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    init {
+        getNextWord()
+    }
+
+   private fun getNextWord(){
+       currentWord = allWordsList.random()
+       val shuffledCurrentWord = currentWord.toCharArray()
+       shuffledCurrentWord.shuffle()
+
+       // shuffled word is the same as original word
+       while (shuffledCurrentWord.joinToString("").equals(currentWord, false))
+           shuffledCurrentWord.shuffle()
+
+
+       // check if the word was already used
+       if (usedWordsList.contains(currentWord)){
+           getNextWord()
+       }else{
+           _currentScrambledWord = shuffledCurrentWord.joinToString("")
+           ++_currentWordCount
+           usedWordsList.add(currentWord)
+       }
+    }
+
+    fun nextWord() : Boolean{
+        return if (_currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
+
+    private fun increaseScore(){
+        _score += SCORE_INCREASE
+    }
+
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            return true
+        }
+        return false
+    }
+
+    fun reinitializeData() {
+        _score = 0
+        _currentWordCount = 0
+        usedWordsList.clear()
+        getNextWord()
+    }
+}
